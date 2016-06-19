7c3b379245b772134e600a9c8a952e1fa4530325
mbjd <balduindettling@gmail.com>
2016-06-19 09:43:06 +0200

    spaces -> tabs

--- bf.c
+++ bf.c
@@ -28,160 +28,153 @@ unsigned char memory[MEMORY_SIZE] = {0};
 
 void interpret(char* program)
 {
-    int ptr = 0;
-    int instruction_ptr = 0;
+	int ptr = 0;
+	int instruction_ptr = 0;
 
-    while (program[instruction_ptr] != '\0')
-    {
-        switch(program[instruction_ptr])
-        {
-            // Simple instructions
-            case '>':
-                ptr++;
-                break;
-            case '<':
-                ptr--;
-                break;
-            case '+':
-                memory[ptr]++;
-                break;
-            case '-':
-                memory[ptr]--;
-                break;
+	while(instruction_ptr < (int) strlen(program))
+	{
+		switch(program[instruction_ptr])
+		{
+			// Simple instructions
+			case '>':
+				ptr++;
+				break;
+			case '<':
+				ptr--;
+				break;
+			case '+':
+				memory[ptr]++;
+				break;
+			case '-':
+				memory[ptr]--;
+				break;
 
-            // I/O instructions
-            case '.':
-                printf("%c", memory[ptr]);
-                break;
-            case ',':
-                printf("\nEnter char to save in cell %d: ", ptr);
-                scanf(" %c", memory + ptr);
-                break;
+				// I/O instructions
+			case '.':
+				printf("%c", memory[ptr]);
+				break;
+			case ',':
+				printf("\nEnter char to save in cell %d: ", ptr);
+				scanf(" %c", memory + ptr);
+				break;
 
-            // Loop/branch instructions
-            case '[':
-                if (memory[ptr] == 0)
-                {
-                    // Jump forward to matching ]
-                    int bracket_counter = 1;
-                    while (bracket_counter)
-                    {
-                        instruction_ptr++;
-                        if (program[instruction_ptr] == '[')
-                        {
-                            bracket_counter++;
-                        }
+				// Loop/branch instructions
+			case '[':
+				if(memory[ptr] == 0)
+				{
+					// Jump forward to matching ]
+					int bracket_ounter = 1;
+					while(bracket_ounter)
+					{
+						instruction_ptr++;
+						if(program[instruction_ptr] == '[')
+						{
+							bracket_ounter++;
+						}
 
-                        if (program[instruction_ptr] == ']')
-                        {
-                            bracket_counter--;
-                        }
-                    }
-                    instruction_ptr--;
-                }
-                break;
+						if(program[instruction_ptr] == ']')
+						{
+							bracket_ounter--;
+						}
+					}
+					instruction_ptr--;
+				}
+				break;
 
-            case ']':
-                if (memory[ptr])
-                {
-                    // Jump back to matching [
-                    int bracket_counter = 1;
-                    while (bracket_counter)
-                    {
-                        instruction_ptr--;
-                        if (program[instruction_ptr] == ']')
-                        {
-                            bracket_counter++;
-                        }
+			case ']':
+				if(memory[ptr] != 0)
+				{
+					// Jump back to matching [
+					int bracket_ounter = 1;
+					while(bracket_ounter)
+					{
+						instruction_ptr--;
+						if (program[instruction_ptr] == ']')
+						{
+							bracket_ounter++;
+						}
 
-                        if (program[instruction_ptr] == '[')
-                        {
-                            bracket_counter--;
-                        }
-                    }
-                }
-                break;
-        }
+						if (program[instruction_ptr] == '[')
+						{
+							bracket_ounter--;
+						}
+					}
+				} // end if
+				break;
+		} // end switch
 
-        instruction_ptr++;
-    }
+		instruction_ptr++;
+	} // end while
 }
 
 void print_memory()
 {
-    puts("\nThe program modified memory as follows:");
-    for (int i = 0; i < MEMORY_SIZE; i++)
-    {
-        // Formatting - extra \n after 8 lines
-        if (i%128 == 0 && i > 0)
-        {
-            printf("\n");
-        }
+	puts("\nThe program modified memory as follows:");
+	for (int i = 0; i < MEMORY_SIZE; i++)
+	{
+		// Formatting - extra \n after 8 lines
+		if(i%128 == 0 && i > 0)
+		{
+			printf("\n");
+		}
 
-        // Print memory positions
-        if (i%16 == 0)
-        {
-            printf("\n0x%04x:  ", i);
-        }
+		// Print memory positions
+		if(i%16 == 0)
+		{
+			printf("\n0x%04x:  ", i);
+		}
 
-        // Print a byte of memory
-        printf("%02x ", memory[i]);
-    }
-    printf("\n");
+		// Print a byte of memory
+		printf("%02x ", memory[i]);
+	}
+	printf("\n");
 }
 
 int main(int argc, char** argv)
 {
-    char* program = calloc(PROGRAM_SIZE, 1);
+	char* program = calloc(PROGRAM_SIZE, 1);
 
-    // Program input
-    if (argc > 1)
-    {
-        // If the user has specified a file, read it
-        FILE* inputFile = fopen(argv[1], "r");
-        if (inputFile == NULL) 
-        {
-            printf("The file \"%s\" could not be opened. Does it exist?\n",
-                argv[1]);
-            return 1;
-        }
-        fread(program, 1, PROGRAM_SIZE, inputFile);
-    }
-    else if (argc == 1)
-    {
-        // If no argument was passed, ask the user to enter some code
-        printf("Enter a BF program: ");
-        scanf("%s", program);
-    }
-
-
-
-    // Start time measurement
-    struct timeval t1, t2;
-    gettimeofday(&t1, NULL);
-
-    // Do the magic
-    interpret(program);
-
-    // Complete time measurement
-    gettimeofday(&t2, NULL);
-
-    // For debugging
-    // print_memory();
-    if (argc == 3)
-    {
-        if (strcmp(argv[2], "m") == 0)
-        {
-            print_memory();
-        }
-    }
-
-    int usec = t2.tv_usec - t1.tv_usec;
-    int sec  = t2.tv_sec - t1.tv_sec;
-    float total = sec + ((float) usec) / 10E6;
-
-    printf("\nElapsed time: %f seconds\n", total);
+	// Program input
+	if (argc > 1)
+	{
+		// If the user has specified a file, read it
+		FILE* inputFile = fopen(argv[1], "r");
+		if(inputFile == NULL)
+		{
+			printf("The file \"%s\" could not be opened. Does it exist?\n",
+					argv[1]);
+			return(1);
+		}
+		fread(program, 1, PROGRAM_SIZE, inputFile);
+	}
+	else if (argc == 1)
+	{
+		// If no argument was passed, ask the user to enter some code
+		printf("Enter a BF program: ");
+		scanf("%s", program);
+	}
 
 
-    return 0;
+
+	// Start time measurement
+	struct timeval t1, t2;
+	gettimeofday(&t1, NULL);
+
+	// Do the magic
+	interpret(program);
+
+	// Complete time measurement
+	gettimeofday(&t2, NULL);
+
+	// For debugging
+	// print_memory();
+
+	int usec = t2.tv_usec - t1.tv_usec;
+	int sec  = t2.tv_sec - t1.tv_sec;
+	float total = sec + ((float) usec) / 10E6;
+
+	printf("\nElapsed time: %f seconds\n", total);
+
+
+	return 0;
 }
